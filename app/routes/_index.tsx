f9c6aab276fc600a504d7930df9775aad72e6bea
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2024-06-09 14:07:37 +0200

    Add Biome

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,19 +2,19 @@ import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
 import stylesheet from "~/globals.css?url";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
+	{ rel: "stylesheet", href: stylesheet },
 ];
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    {
-      name: "description",
-      content: "Welcome to Remix on Cloudflare!",
-    },
-  ];
+	return [
+		{ title: "New Remix App" },
+		{
+			name: "description",
+			content: "Welcome to Remix on Cloudflare!",
+		},
+	];
 };
 
 export default function Index() {
-  return <h1 className="text-2xl">Hello World</h1>;
+	return <h1 className="text-2xl">Hello World</h1>;
 }
